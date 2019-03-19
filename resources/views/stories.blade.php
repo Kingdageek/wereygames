4d179a84ddd2fd139ed6193495af96e9bf85582c
Stanley Ojadovwa <stanwafi@gmail.com>
2019-03-19 13:37:38 +0100

    Updated redirect

--- resources/views/stories.blade.php
+++ resources/views/stories.blade.php
@@ -9,11 +9,11 @@
                         <h2 class="text-capitalize">All Stories</h2>
                     </div>
                 </div>
-                <div class="row mt-5">
+                <div class="row mt-5 text-center">
                    @foreach($stories->chunk(3) as $chunk)
                    <div class="row">
                         @foreach($chunk as $story)
-                          <div class="col-md-4">
+                          <div class="col-md-4 text-center">
                             <a href="{{ route('story.select', $story->id) }}" class="text-white">
                               {{ $story->title }}
                             </a>
