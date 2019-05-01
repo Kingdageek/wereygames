a1b6d6d4e0eca8151383457a17f04fcd0264cff0
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-05-01 14:28:30 +0100

    Controllers to handle wereydictionary

---
+++ app/Http/Controllers/WereyWordController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\WereyWord;
+use Illuminate\Http\Request;
+
+class WereyWordController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\WereyWord  $wereyWord
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WereyWord $wereyWord)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\WereyWord  $wereyWord
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WereyWord $wereyWord)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\WereyWord  $wereyWord
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WereyWord $wereyWord)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\WereyWord  $wereyWord
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WereyWord $wereyWord)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/WordgroupController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Wordgroup;
+use Illuminate\Http\Request;
+
+class WordgroupController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Wordgroup  $wordgroup
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Wordgroup $wordgroup)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Wordgroup  $wordgroup
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Wordgroup $wordgroup)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Wordgroup  $wordgroup
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Wordgroup $wordgroup)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Wordgroup  $wordgroup
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Wordgroup $wordgroup)
+    {
+        //
+    }
+}
