a7db97c5a0650c3cbb6a1ade8d613b5632b2980d
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-05-04 18:46:41 +0100

    methods to CRUD Wordgroups

--- app/Http/Controllers/Admin/WordgroupController.php
+++ app/Http/Controllers/Admin/WordgroupController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Wordgroup;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class WordgroupController extends Controller
@@ -14,7 +15,8 @@ class WordgroupController extends Controller
      */
     public function index()
     {
-        //
+        $wordgroups = Wordgroup::orderBy('created_at', 'desc')->paginate(20);
+        return view('admin.wordgroup.index', compact('wordgroups'));
     }
 
     /**
@@ -24,7 +26,7 @@ class WordgroupController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.wordgroup.create');
     }
 
     /**
@@ -35,13 +37,20 @@ class WordgroupController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|max:50|unique:wordgroups'
+        ]);
+
+        Wordgroup::create([ 'name' => ucfirst($request->name) ]);
+
+        session()->flash('success', 'Wordgroup successfully created');
+        return redirect()->route('admin.wordgroups.index');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Wordgroup  $wordgroup
+     * @param  \App\Models\Wordgroup  $wordgroup
      * @return \Illuminate\Http\Response
      */
     public function show(Wordgroup $wordgroup)
@@ -52,34 +61,42 @@ class WordgroupController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Wordgroup  $wordgroup
+     * @param  \App\Models\Wordgroup  $wordgroup
      * @return \Illuminate\Http\Response
      */
     public function edit(Wordgroup $wordgroup)
     {
-        //
+        return view('admin.wordgroup.edit', compact('wordgroup'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Wordgroup  $wordgroup
+     * @param  \App\Models\Wordgroup  $wordgroup
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Wordgroup $wordgroup)
     {
-        //
+        $this->validate($request, [
+            'name' => 'required|max:50|unique:wordgroups,name,'. $wordgroup->id
+        ]);
+
+        $wordgroup->update([ 'name' => ucfirst( $request->name ) ]);
+        return redirect()->route('admin.wordgroups.index')
+                         ->with('success', 'Wordgroup successfully updated');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Wordgroup  $wordgroup
+     * @param  \App\Models\Wordgroup  $wordgroup
      * @return \Illuminate\Http\Response
      */
     public function destroy(Wordgroup $wordgroup)
     {
-        //
+        $wordgroup->delete();
+        return redirect()->back()
+                         ->with('success', 'Wordgroup <strong>' . $wordgroup->name . '</strong> was successfully deleted');
     }
 }
