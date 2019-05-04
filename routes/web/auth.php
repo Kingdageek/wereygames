49629756d1fddf466aca0c0f15d622ffa6c54492
Nonso Mgbechi <mgbechinonso@gmail.com>
2019-05-04 19:27:06 +0100

    Removed duplicate auth route

--- routes/web/auth.php
+++ routes/web/auth.php
@@ -4,6 +4,6 @@ Route::match(['GET', 'POST'], '/auth/logout')
     ->name('auth.logout')
     ->uses('Auth\LoginController@logout');
 
-Auth::routes();
+// Auth::routes();
 
 Auth::routes(['register' => false]);
